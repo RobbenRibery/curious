7b8b6f36af38e051f70f0fab527df556ca074b34
RobbenRibery <runodng.liu@outlook.com>
2025-04-14 19:22:37 +0100

    enabled partial reward

--- scripts/dr_grpo_dapo.sh
+++ scripts/dr_grpo_dapo.sh
@@ -4,7 +4,7 @@ export PYTORCH_CUDA_ALLOC_CONF="max_split_size_mb:128,expandable_segments:True";
 python3 training.py \
     --wandb_config.project "curious-grpo-gsm8k" \
     --wandb_config.group "drgrp-dapo-ietrate" \
-    --wandb_config.name "drgrpo-dapo-test-prompt[qwen]-reward[no-format]-0kl-adjusted" \
+    --wandb_config.name "drgrpo-dapo-test-prompt[qwen]-reward[no-format]-0kl-partial-reward-wrong-answer" \
     --base_config.model_name "Qwen/Qwen2-0.5B-Instruct" \
     --base_config.device_index 0 \
     --base_config.dataset_name "openai/gsm8k" \
@@ -21,12 +21,12 @@ python3 training.py \
     --base_config.eval_text_log_interval 100 \
     --sampling_config.model_prompt_length 1024 \
     --sampling_config.max_new_tokens 1400 \
-    --sampling_config.temperature 0.7 \
+    --sampling_config.temperature 1.0 \
     --sampling_config.top_p 1.0 \
     --sampling_config.top_k 50 \
     --sampling_config.do_sample \
     --sampling_config.use_cache \
-    --sampling_config.repetition_penalty 1.1 \
+    --sampling_config.repetition_penalty 1.0 \
     --sampling_config.system_prompt "qwen_system_prompt" \
     --reward_config.no-use-format-reward \
     --grpo_config.group_size 16 \
