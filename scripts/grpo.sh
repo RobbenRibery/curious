1c25194c41566484b8c6cb9d303d77d5ff643d56
Rundong Liu <rundong.liu@outlook.com>
2025-04-26 19:58:05 +0000

    learning rate > 1e-06 seems still very problmatic

--- scripts/grpo.sh
+++ scripts/grpo.sh
@@ -6,7 +6,7 @@ export TORCHDYNAMO_CAPTURE_SCALAR_OUTPUTS=1;
 python -m curious.training.train_rl \
     --wandb_config.project "curious-gsm8k" \
     --wandb_config.group "grpo-improve" \
-    --wandb_config.name "[grpo-padtoken-fixed]-grpo-qwen25-prompt[qwen]-reward[partial-solved-penalize-trailing]-temp1rep11-5e06rl-bsz16" \
+    --wandb_config.name "[grpo-padtoken-fixed]-grpo-qwen25-prompt[qwen]-reward[partial-solved-penalize-trailing]-temp1-5e06rl-epochperstep8-grad1-bsz16" \
     --base_config.model_name "Qwen/Qwen2.5-0.5B-Instruct" \
     --base_config.device_index 0 \
     --base_config.dataset_name "openai/gsm8k" \
@@ -23,20 +23,20 @@ python -m curious.training.train_rl \
     --base_config.eval_text_log_interval 50 \
     --sampling_config.model_prompt_length 1024 \
     --sampling_config.max_new_tokens 512 \
-    --sampling_config.temperature 1.0 \
+    --sampling_config.temperature 1.2 \
     --sampling_config.top_p 1.0 \
-    --sampling_config.top_k 50 \
+    --sampling_config.top_k 0 \
     --sampling_config.do_sample \
     --sampling_config.use_cache \
-    --sampling_config.repetition_penalty 1.1 \
+    --sampling_config.repetition_penalty 1.0 \
     --sampling_config.system_prompt "qwen_system_prompt" \
     --reward_config.no-use-format-reward \
     --reward_config.no-use-overlong-penalty \
     --grpo_config.group_size 16 \
-    --grpo_config.lr 5e-06 \
+    --grpo_config.lr 2e-06 \
     --grpo_config.weight_decay 0.0 \
-    --grpo_config.kl_weight 0.0 \
+    --grpo_config.kl_weight 0.001 \
     --grpo_config.clip_eps 0.2 \
     --grpo_config.mini_batch_size 64 \
-    --grpo_config.epochs_per_step 1 \
+    --grpo_config.epochs_per_step 8 \
     --grpo_config.max_grad_norm 1.0 \
